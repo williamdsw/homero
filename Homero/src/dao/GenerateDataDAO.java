3f0f8a27694fd402976fe188479c2ba84008e9a3
William Santos <47191495+williamdsw@users.noreply.github.com>
2019-02-10 14:58:25 -0200

    Update GenerateDataDAO.java

--- Homero/src/dao/GenerateDataDAO.java
+++ Homero/src/dao/GenerateDataDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import java.util.ArrayList;
@@ -15,43 +10,6 @@ import model.GraphicParameterMODEL;
  */
 public class GenerateDataDAO
 {
-    
-    /**
-     * Generates data for age range with random values
-     * @return 
-     */
-    public Collection<GraphicParameterMODEL> generateByAgeRange ()
-    {
-        /* List of age ranges */
-        String[] ageRanges = { "21 - 25", "26 - 30", "31 - 35", "36 - 40", "41 - 45" };
-        
-        /* List of models */
-        Collection<GraphicParameterMODEL> listModels = new ArrayList<> ();
-        
-        try
-        {
-            for (String ageRenge : ageRanges)
-            {
-                /* New instance */
-                GraphicParameterMODEL model = new GraphicParameterMODEL ();
-                
-                /* Passing data */
-                model.setColumnLabel (ageRenge);
-                model.setRowLabel (ageRenge);
-                model.setValue (Math.random () * 1000000);
-                
-                /* Filling in list */
-                listModels.add (model);
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.println (e.getMessage ());
-        }
-        
-        return listModels;
-    }
-    
     /**
      * Generates data from array of string passed and generate a random value based on min / max values
      * @param arrItems
